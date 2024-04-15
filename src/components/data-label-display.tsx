e4a4b196f5b341326e4ffd0b880a152494141be0
Yuri Almeida Rodrigue <yuri_a.rodrigues@hotmail.com>
2024-04-15 16:29:48 -0300

    feat: adds data display components

---
+++ src/components/data-label-display.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@mui/joy";
+
+type DataLabelDisplay = {
+	data: string;
+	label: string;
+};
+
+export const DataLabelDisplay = ({ data, label }: DataLabelDisplay) => {
+	return (
+		<Box>
+			<Typography
+				component="h2"
+				fontSize={22}
+				fontWeight="500"
+				textColor="rgba(0, 0, 0, 1)"
+			>
+				{data}
+			</Typography>
+			<Typography
+				component="h4"
+				fontSize={14}
+				lineHeight="1.25rem"
+				fontWeight="400"
+				textColor="rgba(0, 0, 0, 0.4)"
+			>
+				{label}
+			</Typography>
+		</Box>
+	);
+};
