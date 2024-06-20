62f88f10a1991fccb64a374cad2deac2805065c5
Yuri Almeida Rodrigue <yuri_a.rodrigues@hotmail.com>
2024-06-20 12:14:10 -0300

    feat: adds new colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,15 +3,16 @@ export type ColorType = {
 };
 
 export const colors = {
-	"black-50": "#262a41",
-	"black-100": "#1D1E24",
-	"black-300": "rgba(10, 10, 10, 0.5)",
-	"black-500": "#101010",
+	'black-50': '#262a41',
+	'black-100': '#1D1E24',
+	'black-300': 'rgba(10, 10, 10, 0.5)',
+	'black-500': '#101010',
 
-	"gray-100": "#f5f5fa",
-	"gray-300": "#EBEBF0",
-	"gray-700": "#888888",
-	"gray-900": "rgba(29, 30, 36, 0.5)",
+	'gray-100': '#f5f5fa',
+	'gray-200': '#F0F0F0',
+	'gray-300': '#EBEBF0',
+	'gray-700': '#888888',
+	'gray-900': 'rgba(29, 30, 36, 0.5)',
 
-	"purple-50": "#4339f2",
+	'purple-50': '#4339f2',
 };
